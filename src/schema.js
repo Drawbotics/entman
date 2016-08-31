6576123ba2fe02adb185d07d7ed6a8e21971e8d0
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-08-31 18:31:47 +0200

    Implement tests to defineSchema and hasMany

--- src/schema.js
+++ src/schema.js
@@ -1,8 +1,32 @@
-export function defineSchema(name, attributes) {
+import isEmpty from 'lodash/isEmpty';
+import { Schema, arrayOf } from 'normalizr';
+
+
+export function defineSchema(name, attributes={}) {
+  if (isEmpty(name)) {
+    throw new Error('[INVALID NAME]');
+  }
+  if ((typeof attributes !== 'object') && attributes) {
+    throw new Error('[INVALID ATTRIBUTES]');
+  }
+  return {
+    name,
+    attributes,
+  };
 }
 
 
-export function hasMany() {
+export function hasMany(schema) {
+  if (isEmpty(schema)) {
+    throw new Error('[INVALID SCHEMA]');
+  }
+  if (typeof schema !== 'string' && ! schema.hasOwnProperty('name')) {
+    throw new Error('[INVALID SCHEMA]');
+  }
+  return {
+    relatedEntity: schema.hasOwnProperty('name') ? schema.name : schema,
+    isArray: true,
+  };
 }
 
 
--- test/unit/schema.js
+++ test/unit/schema.js
@@ -1,12 +1,60 @@
-define('@Schema', function () {
+import { expect } from 'chai';
+
+import {
+  defineSchema,
+  hasMany,
+  generateSchema,
+} from 'schema';
+
+
+describe('@Schema', function () {
   describe('defineSchema(name, attributes)', function () {
-    it('should throw an error when `name` is empty');
-    it('should throw an error when `attributes` is not an object');
-    it('should return a schema object');
-    it('the schema object should have a property with the name of the schema');
-    it('the schema object should have a property with the attributes of the schema');
+    it('should throw an error when `name` is empty', function () {
+      expect(() => defineSchema()).to.throw(/INVALID NAME/);
+    });
+    it('should throw an error when `attributes` is a function', function () {
+      expect(() => defineSchema('Test', () => {})).to.throw(/INVALID ATTRIBUTES/);
+      expect(() => defineSchema('Test', 123)).to.throw(/INVALID ATTRIBUTES/);
+    });
+    it('should return an object', function () {
+      const result = defineSchema('Test');
+      expect(result).to.be.an('object');
+    });
+    it('the schema object should have a property with the name of the schema', function () {
+      const name = 'Test';
+      const result = defineSchema(name);
+      expect(result.name).to.equal(name);
+    });
+    it('the schema object should have a property with the attributes of the schema', function () {
+      const name = 'Test';
+      const attributes = { user: 'User' };
+      const result = defineSchema(name, attributes);
+      expect(result.attributes).to.deep.equal(attributes);
+    });
   });
-  describe('hasMany()', function () {
+  describe('hasMany(schema)', function () {
+    it('should throw an error when `schema` is empty', function () {
+      expect(() => hasMany()).to.throw(/INVALID SCHEMA/);
+    });
+    it('should throw an error if `schema` is not a string or a schema object', function () {
+      expect(() => hasMany(123)).to.throw(/INVALID SCHEMA/);
+      expect(() => hasMany({})).to.throw(/INVALID SCHEMA/);
+      expect(() => hasMany('User')).to.not.throw(/INVALID SCHEMA/);
+      expect(() => hasMany({ name: 'User' })).to.not.throw(/INVALID SCHEMA/);
+    });
+    it('should return an object', function () {
+      const result = hasMany('User');
+      expect(result).to.be.an('object');
+    });
+    it('the resulted object should have a property called `relatedEntity` with the name of the passed schema', function () {
+      const name = 'User';
+      const result = hasMany(name);
+      expect(result.relatedEntity).to.equal(name);
+    });
+    it('the resulted object should have a property called `isArray` with value `true`', function () {
+      const result = hasMany('User');
+      expect(result.isArray).to.equal(true);
+    });
   });
   describe('generateSchemas()', function () {
   });
