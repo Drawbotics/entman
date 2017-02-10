484e13a6a31b7f2904c09138983e448f4decb150
Lorenzo Ruiz <lars.bs@hotmail.com>
2017-02-10 11:13:52 +0100

    Refactor reducer code

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import omit from 'lodash/omit';
 import isEmpty from 'lodash/isEmpty';
 import get from 'lodash/get';
@@ -29,116 +30,78 @@ function updateEntities(state, action) {
 
 
 function updateEntitiesIds(state, action) {
+  const { ids } = action.payload;
+  /*
+    const { key, schema, oldId, newId } = action.payload;
+    const updatedEntity = { ...state[key][oldId], id: newId };
+    return mapValues(state, (currentEntities, key) => {
+      // The entities we need to update
+      if (key === schema.getKey()) {
+        return omit({
+          ...currentEntities,
+          [newId]: updatedEntity,
+        }, oldId);
+      }
+      // No related entities, so no need to perform any update
+      if ( ! schema.isRelatedTo(key)) {
+        return currentEntities;
+      }
+      // The entities are related
+      const { isParent, relatedPropName } = schema.getRelation(updatedEntity, key);
+      if (isParent) {
+        // TODO: The entity is a OneToOne relation
+        return currentEntities;
+      }
+      // The entities are the 'many' part in a OneToMany relation
+      return mapValues(currentEntities, (entity) => {
+        if (entity[relatedPropName] !== oldId) {
+          return entity;
+        }
+        return {
+          ...entity,
+          [relatedPropName]: newId,
+        };
+      });
+    });
+  */
 }
 
 
 function deleteEntities(state, action) {
   const { ids } = action.payload;
-  console.log(omit(state, ids));
   return omit(state, ids);
 }
 
 
-function reducer(state, action) {
-  if ( ! get(action, 'meta.entmanAction', false)) return state;
-  switch (action.meta.type) {
-    case EntitiesActions.CREATE_ENTITIES:
-    case EntitiesActions.CREATE_ENTITY: {
-      const { entities } = action.payload.data;
-      return mapValues(state, (currentEntities, key) => ({
-        ...currentEntities,
-        ...entities[key],
-      }));
+function createEntityReducer(entitySchema) {
+  const entityName = entitySchema.getKey();
+  return (state={}, action) => {
+    switch (action.type) {
+      case `@@entman/CREATE_ENTITIES_${entityName.toUpperCase()}`: {
+        return createEntities(state, action);
+      }
+      case `@@entman/UPDATE_ENTITIES_${entityName.toUpperCase()}`: {
+        return updateEntities(state, action);
+      }
+      case `@@entman/DELETE_ENTITIES_${entityName.toUpperCase()}`: {
+        return deleteEntities(state, action);
+      }
+      case `@@entman/UPDATE_ENTITIES_IDS_${entityName.toUpperCase()}`: {
+        return updateEntitiesIds(state, action);
+      }
+      default:
+        return state;
     }
-    case EntitiesActions.UPDATE_ENTITY: {
-      const { data, id, key, useDefault } = action.payload;
-      const newData = data.entities[key][id];
-      delete newData.id;
-      return {
-        ...state,
-        [key]: {
-          ...state[key],
-          [id]: useDefault ? defaultTo(state[key][id], newData) : update(state[key][id], newData),
-        },
-      };
-    }
-    case EntitiesActions.UPDATE_ENTITIES: {
-      const { data, ids, key } = action.payload;
-      return ids.reduce((memo, id) => {
-        const newData = data.entities[key][id];
-        delete newData.id;
-        return {
-          ...memo,
-          [key]: {
-            ...memo[key],
-            [id]: update(memo[key][id], newData),
-          },
-        };
-      }, state);
-    }
-    case EntitiesActions.UPDATE_ENTITY_ID: {
-      const { key, schema, oldId, newId } = action.payload;
-      const updatedEntity = { ...state[key][oldId], id: newId };
-      return mapValues(state, (currentEntities, key) => {
-        // The entities we need to update
-        if (key === schema.getKey()) {
-          return omit({
-            ...currentEntities,
-            [newId]: updatedEntity,
-          }, oldId);
-        }
-        // No related entities, so no need to perform any update
-        if ( ! schema.isRelatedTo(key)) {
-          return currentEntities;
-        }
-        // The entities are related
-        const { isParent, relatedPropName } = schema.getRelation(updatedEntity, key);
-        if (isParent) {
-          // TODO: The entity is a OneToOne relation
-          return currentEntities;
-        }
-        // The entities are the 'many' part in a OneToMany relation
-        return mapValues(currentEntities, (entity) => {
-          if (entity[relatedPropName] !== oldId) {
-            return entity;
-          }
-          return {
-            ...entity,
-            [relatedPropName]: newId,
-          };
-        });
-      });
-    }
-    case EntitiesActions.DELETE_ENTITY: {
-      const { key, id } = action.payload;
-      return {
-        ...state,
-        [key]: omit(state[key], id),
-      };
-    }
-    default:
-      return state;
   }
 }
 
 
-function createReducer(entities) {
-  const reactions = Object.keys(entities).reduce((memo, k) => ({
+function createReducer(schemas) {
+  const entitiesReducers = Object.keys(schemas).reduce((memo, k) => ({
     ...memo,
-    [`@@entman/CREATE_ENTITIES_${k.toUpperCase()}`]: (state, action) => ({
-      ...state, [k]: createEntities(state[k], action),
-    }),
-    [`@@entman/UPDATE_ENTITIES_${k.toUpperCase()}`]: (state, action) => ({
-      ...state, [k]: updateEntities(state[k], action),
-    }),
-    [`@@entman/DELETE_ENTITIES_${k.toUpperCase()}`]: (state, action) => ({
-      ...state, [k]: deleteEntities(state[k], action),
-    }),
+    [k]: createEntityReducer(schemas[k]),
   }), {});
-  return (state, action) => {
-    const reaction = reactions[action.type];
-    return (typeof reaction === 'function') ? reaction(state, action) : state;
-  };
+  return combineReducers(entitiesReducers);
 }
 
 
@@ -146,6 +109,5 @@ export default function entities(schemas) {
   if (isEmpty(schemas)) {
     throw new Error('[INVALID SCHEMAS]');
   }
-  const emptyEntities = mapValues(schemas, () => ({}));
-  return (state=emptyEntities, action) => createReducer(emptyEntities)(state, action);
+  return createReducer(schemas);
 }
