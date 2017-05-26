324d939657d87878941658a3e6250c4e462bf27d
Lorenzo Ruiz <lars.bs@hotmail.com>
2017-05-26 15:46:14 +0200

    Allow posibility of getting raw entities from selectors

--- src/selectors.js
+++ src/selectors.js
@@ -9,31 +9,46 @@ export function getEntitiesSlice(state) {
 }
 
 
-export function getEntities(state, schema, ids) {
+export function getEntities(state, schema, ids, raw) {
   const key = schema.key;
   const entitiesState = getEntitiesSlice(state);
   const entities = ids ?
     values(entitiesState[key]).filter(e => ids.includes(e.id)) : values(entitiesState[key]);
+
+  if (raw) {
+    return entities;
+  }
+
   return entities.map(e => denormalize(e, entitiesState, schema));
 }
 
 
-export function getEntitiesBy(state, schema, by={}) {
+export function getEntitiesBy(state, schema, by={}, raw) {
   const byKey = Object.keys(by)[0];
   const value = by[byKey];
   const key = schema.key;
   const entitiesState = getEntitiesSlice(state);
   const entities = values(pickBy(entitiesState[key], e => e[byKey] === value));
+
+  if (raw) {
+    return entities;
+  }
+
   return entities.map(e => denormalize(e, entitiesState, schema));
 }
 
 
-export function getEntity(state, schema, id) {
+export function getEntity(state, schema, id, raw) {
   if ( ! id) throw new Error('Required param `id` is missing');
   const key = schema.key;
   const entitiesState = getEntitiesSlice(state);
   const entities = entitiesState[key];
   if (isEmpty(entities)) return null;
   const entity = entities[id];
+
+  if (raw) {
+    return entity;
+  }
+
   return denormalize(entity, entitiesState, schema);
 }
